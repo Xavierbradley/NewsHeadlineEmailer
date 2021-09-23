1e4d653240e0d33ae069f4d406262b9538cfa507
Xavier Bradley <xbradley@rlliquidators.com>
2021-09-23 09:25:21 -0700

    Finished the extraction of the news from Hackernews

--- app.py
+++ app.py
@@ -24,4 +24,19 @@ content = ''
 def extract_news(url):
     print('Extracting News Stories')
     cnt = ''
-    cnt +=('<b>Top Stories:</>\n' + '<br> ')
+    cnt +=('<b>Top Stories:</>\n' + '<br>' + '-' * 50 + '<br>')
+    response = request.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(soup.find_all('td', attrs={'class':'title', 'valign' : ''})):
+        cnt += ((str(i + 1) + ' :: ' + '+tag.text + "/n" + '<br>' ) if tag.text!= 'More' else '')
+    return(cnt)
+
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>-------<br>')
+content += ('<br><br>End of Message')
+
+
+#email sending
+print('Composing email')
